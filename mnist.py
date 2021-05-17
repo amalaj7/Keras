761e2bdca2086fb7d27ded18880a2dff4505073b
Amal Ajay <60337485+amalaj7@users.noreply.github.com>
2021-05-17 12:58:17 +0530

    mnist-dataset

---
+++ mnist.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(X_train_full, y_train_full), (X_test, y_test) = mnist.load_data()
+
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+
+# scale the test set as well
+X_test = X_test / 255.
+
+LAYERS = [tf.keras.layers.Flatten(input_shape=[28, 28], name="inputLayer"),
+          tf.keras.layers.Dense(300, activation="relu", name="hiddenLayer1"),
+          tf.keras.layers.Dropout(0.5),
+          tf.keras.layers.Dense(100, activation="relu", name="hiddenLayer2"),
+          tf.keras.layers.Dense(10, activation="softmax", name="outputLayer")]
+
+model_clf = tf.keras.models.Sequential(LAYERS)
+
+LOSS_FUNCTION = "sparse_categorical_crossentropy"
+OPTIMIZER = "adam"
+METRICS = ["accuracy"]
+
+model_clf.compile(loss=LOSS_FUNCTION,
+              optimizer=OPTIMIZER,
+              metrics=METRICS)
+
+EPOCHS = 10
+VALIDATION_SET = (X_valid, y_valid)
+
+history = model_clf.fit(X_train, y_train, epochs=EPOCHS,
+                    validation_data=VALIDATION_SET)
+model_clf.evaluate(X_test, y_test)
+
+
+
+
+
+
